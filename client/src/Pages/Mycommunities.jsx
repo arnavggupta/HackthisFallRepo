911837a2f0a7538e4f0b83549a1238c2d1fc023d
soham haldar <sohamhaldar25@gmail.com>
2024-02-10 11:38:00 +0530

    added

--- client/src/Pages/Mycommunities.jsx
+++ client/src/Pages/Mycommunities.jsx
@@ -6,7 +6,7 @@ function Community() {
 <div className="grid grid-rows-auto-1fr">
   <div>
     <LandingNavbar />
-    <div style={{ display: 'flex', flexDirection: 'column' }} className="h-screen w-screen">
+    <div style={{ display: 'flex', flexDirection: 'column' }} className="h-screen w-screen flex justify-center">
       <div style={{ display: 'flex', justifyContent: 'space-around', marginBottom: '40px' }}>
         <Cards />
         <Cards />
